bb93e50e77dddfe180d1fda6eb6bfc5399039124
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-03-27 04:39:57 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -8,34 +8,34 @@
 
 class Automata {
 public:
-States state;
-int cash;
-std::map <int, std::string> menu;
-std::map <int, int> prices;
-int choice;
-Automata() {
-this->state = OFF;
-this->cash = 0;
-this->choice = -1;
-menu[0] = "Латте";
-menu[1] = "Капучино";
-menu[2] = "Эспрессо";
-prices[0] = 50;
-prices[1] = 60;
-prices[2] = 70;
-}
-void on();
-void off();
-void coin(); 
-void coin(int);
-void etMenu(std::map <int, std::string>, std::map <int, int>);
-void etMenu(std::string, std::string);
-States getState();
-int getCash();
-void choiceuser();
-void choiceuser(int);
-void check();
-void cancel();
-void cook();
-void finish();
+ States state;
+ int cash;
+ std::map <int, std::string> menu;
+ std::map <int, int> prices;
+ int choice;
+ Automata() {
+ this->state = OFF;
+ this->cash = 0;
+ this->choice = -1;
+ menu[0] = "Латте";
+ menu[1] = "Капучино";
+ menu[2] = "Эспрессо";
+ prices[0] = 50;
+ prices[1] = 60;
+ prices[2] = 70;
+ }
+ void on();
+ void off();
+ void coin();
+ void coin(int);
+ void etMenu(std::map <int, std::string>, std::map <int, int>);
+ void etMenu(std::string, std::string);
+ States getState();
+ int getCash();
+ void choiceuser();
+ void choiceuser(int);
+ void check();
+ void cancel();
+ void cook();
+ void finish();
 };
